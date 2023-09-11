f778e2aa7308f56d7efd2a75745c8de3ced12376
sinned8 <dennisdust2@gmail.com>
2023-09-11 18:51:36 -0400

    Added a hover effect to the divs that changes the background color to red

--- java.js
+++ java.js
@@ -6,10 +6,17 @@ function makeGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (var i = 0; i < (rows * cols); i++){
-        var box = document.createElement('div');
+        let box = document.createElement('div');
         box.className = 'box';
         container.appendChild(box)
     }
 }
 
 makeGrid(16, 16);
+
+const box = document.querySelectorAll('.box');
+[...box].forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.classList.add('hover');
+    });
+})
