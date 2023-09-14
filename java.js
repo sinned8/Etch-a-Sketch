715fb6c62aea6cbc48253a0e421d0477e331830e
sinned8 <dennisdust2@gmail.com>
2023-09-14 13:53:30 -0400

    Added container to properly align items in the webpage, added some additional CSS classes as well. Basic outline complete

--- java.js
+++ java.js
@@ -1,24 +1,22 @@
+
+//dec global var
 const container = document.querySelector('#container');
 let gridsize;
 
 
-
+//set button up to let user choose size of grid
 document.querySelector('#promptBttn').addEventListener('click', function (e) {
     let gridsize = 10;
     do {
         gridsize = +prompt('Enter a grid size [max: 100]')
     } while (gridsize > 100)
-    
     makeGrid(gridsize);
-
 })
-// let rows = gridsize;
-// let cols = gridsize;
 
 
+//create grid and also add mouse over listener that calls colorGrid func
 function makeGrid(gridsize){
     container.innerHTML = '';
-   
     for (var i = 0; i < (gridsize * gridsize); i++){
         let box = document.createElement('div');
         box.classList.add('box');
@@ -29,10 +27,15 @@ function makeGrid(gridsize){
     }
 }
 
+
+//add the class hover to each box
 function colorGrid(e){
             e.target.classList.add('hover');
     }
 
+
+
+// const reset button and clear the hover effect
 const resetBttn = document.querySelector('#resetBttn');
 function resetGrid(){
     let box = container.querySelectorAll('div');
@@ -41,13 +44,17 @@ function resetGrid(){
 resetBttn.addEventListener('click', ()=>resetGrid());
 
 
+// const bwBttn = document.querySelector('#bwMode');
+// function bw(){
+//     let box = container.querySelectorAll('div');
+//     box.forEach(box=> box.style.backgroundColor='red');
+
+// }
+// bwBttn.addEventListener('click', ()=>bw());
+
+
+// set the default grid size to 16 upon window loading
 window.onload = makeGrid(16);
 
-// const box = document.querySelectorAll('.box');
-// [...box].forEach(box => {
-//     box.addEventListener('mouseover', () => {
-//         box.classList.add('hover');
-//     });
-// })
 
 
